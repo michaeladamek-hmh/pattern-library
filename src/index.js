45e2e15d1f2ba7ec54d5e65a0b9ecbd93aed2c7a
Michael Adamek <michael.adamek@hmhco.com>
2022-09-12 10:35:24 -0400

    Default files removed/updated.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './source.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
