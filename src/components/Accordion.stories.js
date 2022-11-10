7e9bd74881672c382ae17aa4a2252e6ea6ecfcb5
Michael Adamek <michael.adamek@hmhco.com>
2022-11-10 15:01:56 -0500

    Updated placeholder text.

--- src/components/Accordion.stories.js
+++ src/components/Accordion.stories.js
@@ -13,8 +13,8 @@ const Template = args => <Accordion {...args} />;
 export const Default = Template.bind({});
 Default.args = {
     listItems : [
-        {...AccordionListItemStories.Default.args.item, id: '1', title: 'A New Accordion Title', text: 'Lorem Ipsum...', open: true},
-        {...AccordionListItemStories.Default.args.item, id: '2', title: 'A New Accordion Title', text: 'Lorem Ipsum...', image: true},
-        {...AccordionListItemStories.Default.args.item, id: '3', title: 'A New Accordion Title', text: 'Lorem Ipsum...'}
+        {...AccordionListItemStories.Default.args.item, id: '1', title: 'A New Accordion Title', text: 'This is a paragraph of placeholder text. It is only here to help show the layout of the page and how the text will flow. Replace this placeholder text with your own meaningful content.', open: true},
+        {...AccordionListItemStories.Default.args.item, id: '2', title: 'A New Accordion Title', text: 'This is a paragraph of placeholder text. It is only here to help show the layout of the page and how the text will flow. Replace this placeholder text with your own meaningful content.', image: true},
+        {...AccordionListItemStories.Default.args.item, id: '3', title: 'A New Accordion Title', text: 'This is a paragraph of placeholder text. It is only here to help show the layout of the page and how the text will flow. Replace this placeholder text with your own meaningful content.'}
     ]
 };
